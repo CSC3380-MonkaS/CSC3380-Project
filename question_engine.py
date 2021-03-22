32c2def260db1cb7c50795f9430c01ee7ebe94b1
Kayla Conner <63123938+kconn26@users.noreply.github.com>
2021-03-21 20:37:07 -0500

    UI changes

--- question_engine.py
+++ question_engine.py
@@ -29,13 +29,19 @@ def question_engine(question_file):
     for idx in range(len(questions)):
 
         layout_column = [[sg.Input(visible=False)],
-                         [sg.Text(questions[idx].getQ()[0], justification='center', font="Fixedsys 20")],
-                         [sg.Button(questions[idx].getQ()[1][0], button_color='Blue', key="-O1-", font="Fixedsys 20", size=(20, 10)),
-                          sg.Button(questions[idx].getQ()[1][1], button_color="red", key="-O2-", font="Fixedsys 20", size=(20, 10))]]
+                        [sg.Text()],
+                        [sg.Text()],
+                        [sg.Text(questions[idx].getQ()[0], justification='center', font="Fixedsys 30")],
+                        [sg.Text()],
+                        [sg.Text()],
+                        [sg.Text()],
+                        [sg.Text()],
+                        [sg.Button(questions[idx].getQ()[1][0], button_color='Blue', key="-O1-", font="Fixedsys 20", size=(13, 8)),
+                        sg.Button(questions[idx].getQ()[1][1], button_color="red", key="-O2-", font="Fixedsys 20", size=(13, 8))]]
 
         layout = [[sg.Column(layout_column, element_justification='center')]]
 
-        window = sg.Window("MonkaS Movie Recommendation System", layout)
+        window = sg.Window("MonkaS Movie Recommendation System", layout, size=(800, 500))
 
         while True:
             event, values = window.read()
