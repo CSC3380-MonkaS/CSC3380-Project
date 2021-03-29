e08cf53e9c37520220bde8e5b3b803c66b6f3eea
Chris Vasquez <56667015+chrispvasquez@users.noreply.github.com>
2021-03-29 12:12:46 -0500

    Updated to fit object oriented design (not main.py)

---
+++ CSC3380-Project-windows-version/helper_functions.py
@@ -0,0 +1,58 @@
+# Converts a list to a string
+# with spaces between indices
+#
+#
+# function: convert
+#
+# returns:
+#          A string of the indices in the list with spaces
+#          placed between each one
+#
+# parameters:
+#          question_file [string] The input question text file
+#
+# @author MonkaS
+# @since 3/11/2021
+
+def convert(l):
+    return ', '.join(l)
+
+
+# Finds the intersect between two sets of data
+#
+#
+# function: intersect
+#
+# returns:
+#          A list of the intersect of two sets of data
+#
+# parameters:
+#          a [string[]] The first set of data
+#          b [string[]] The isecond set of data
+#
+# @author MonkaS
+# @since 3/11/2021
+def intersect(a, b):
+    return list(set(a) & set(b))
+
+#  Converts list to string
+#
+# function: list_to_string
+#
+# returns:
+#           str - A string
+#
+# parameters:
+#           s [list] A list
+#
+# @author MonkaS
+# @since 3/11/2021
+
+def listToString(s):
+
+    str = ""
+
+    for i in s:
+        str += i
+
+    return str
